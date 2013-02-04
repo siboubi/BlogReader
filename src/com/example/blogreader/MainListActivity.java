6a5fabdaed4d202ccbe54f0787355937d29a40f2
Steve Iboubi <steve.iboubibangole@us.sogeti.com>
2013-02-03 21:28:50 -0600

    more stuffs

--- src/com/example/blogreader/MainListActivity.java
+++ src/com/example/blogreader/MainListActivity.java
@@ -1,18 +1,18 @@
 package com.example.blogreader;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.ListActivity;
-import android.content.ContentProvider;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
 	protected String[] mBlogPostTitle;
-
+	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +20,12 @@ public class MainListActivity extends ListActivity {
         // We'll define a custom screen layout here (the one shown above), but
         // typically, you could just use the standard ListActivity layout.
         setContentView(R.layout.activity_main_list);
+        
+        try {
+			URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/getrecentsummary/?count="+NUMBER_OF_POSTS);
+		} catch (MalformedURLException e) {
+			Log.e(TAG, "Exception caught: ", e);
+		}
         
 //        Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG);
     }
