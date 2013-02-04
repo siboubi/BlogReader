a868a2d109a618f3f9a23252af60e8d9ceb72290
Steve Iboubi <steve.iboubibangole@us.sogeti.com>
2013-02-04 13:02:37 -0600

    added steam reader

--- src/com/example/blogreader/MainListActivity.java
+++ src/com/example/blogreader/MainListActivity.java
@@ -1,12 +1,22 @@
 package com.example.blogreader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
 	protected String[] mBlogPostTitle;
@@ -20,21 +30,69 @@ public class MainListActivity extends ListActivity {
         // We'll define a custom screen layout here (the one shown above), but
         // typically, you could just use the standard ListActivity layout.
         setContentView(R.layout.activity_main_list);
-        
-        try {
-			URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/getrecentsummary/?count="+NUMBER_OF_POSTS);
-		} catch (MalformedURLException e) {
-			Log.e(TAG, "Exception caught: ", e);
+        if (isNetworkAvailable()) {
+			GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+			getBlogPostsTask.execute();
+		}
+        else {
+			Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
 		}
         
-//        Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG);
+//        Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
     }
 
-    @Override
+    private boolean isNetworkAvailable() {
+    	ConnectivityManager connectivyManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    	NetworkInfo networkInfo = connectivyManager.getActiveNetworkInfo();
+    	boolean isAvalable = false;
+    	if (networkInfo != null && networkInfo.isConnected()) {
+    		isAvalable = true;
+		}
+    	return isAvalable;
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main_list, menu);
         return true;
     }
     
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+		@Override
+		protected String doInBackground(Object... params) {
+			int responseCode = -1;
+			
+	        try {
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count="+NUMBER_OF_POSTS);
+				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+				connection.connect();
+				
+				responseCode = connection.getResponseCode();
+				if (responseCode == HttpURLConnection.HTTP_OK) {
+					InputStream inputStream = connection.getInputStream();
+					Reader reader = new InputStreamReader(inputStream);
+					int connectionLength = connection.getContentLength();
+					char[] charArray = new char[connectionLength];
+					reader.read(charArray);
+					String responseData = new String(charArray);
+					Log.v(TAG,responseData);
+				} else {
+					Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+				}
+			} 
+	        catch (MalformedURLException e) {
+				Log.e(TAG, "Exception caught: ", e);
+			}
+	        catch (IOException e) {
+				Log.e(TAG, "Exception caught: ", e);
+			}
+	        catch (Exception e) {
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			return "Code: "+responseCode;
+		}
+    	
+    }
 }
